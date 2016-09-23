edc189581512fe2cd5b45e320d56fc6af1a65e9a
Tim Mecklem <tim@teamgaslight.com>
2016-09-22 21:31:37 -0400

    Temporary workaround to get ssl going

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
 class PagesController < ApplicationController
-
+  def letsencrypt
+    render text: 'gTrmpuvlhFtL3v0N2Rkhk9GBxkzXsnkfwyf_-XWRsj0.3agPbEGMW8yyXAdNJmtYhleq07pUgmnN1oCrhN9iRwA'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
   get "/*action", controller: :pages, as: :page
   root to: "pages#home"
 end
