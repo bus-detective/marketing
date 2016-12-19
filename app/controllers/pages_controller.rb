7a82f1453122c5cb89a23719d511d42412063384
Bill Barnett <bill@gaslight.co>
2016-12-19 14:49:41 -0500

    Add route for Let’s Encrypt validation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
   def letsencrypt
-    if params[:id] == "gTrmpuvlhFtL3v0N2Rkhk9GBxkzXsnkfwyf_-XWRsj0"
+    case params[:id]
+    when 'LKgzYKUJjp71CZ5s9PUA6_cj6xebp_u7VmTLX4NDun8'
+      render text: 'LKgzYKUJjp71CZ5s9PUA6_cj6xebp_u7VmTLX4NDun8.x2TXuRtPY5PkPL4YMeiKaMl4xBtFrjfOe94AR0Iyg1M'
+    when "gTrmpuvlhFtL3v0N2Rkhk9GBxkzXsnkfwyf_-XWRsj0"
       render text: 'gTrmpuvlhFtL3v0N2Rkhk9GBxkzXsnkfwyf_-XWRsj0.3agPbEGMW8yyXAdNJmtYhleq07pUgmnN1oCrhN9iRwA'
-    elsif params[:id] == "OLj4hja-R9m7nOL2UWmj8jG-WlnX7Y2XJqGPEJ_WGXc"
+    when == "OLj4hja-R9m7nOL2UWmj8jG-WlnX7Y2XJqGPEJ_WGXc"
       render text: 'OLj4hja-R9m7nOL2UWmj8jG-WlnX7Y2XJqGPEJ_WGXc.3agPbEGMW8yyXAdNJmtYhleq07pUgmnN1oCrhN9iRwA'
+    else
+      head :not_found
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
   get "/*action", controller: :pages, as: :page
   root to: "pages#home"
 end
