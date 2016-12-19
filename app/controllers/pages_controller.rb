539251d760012440eba0f7c5554c37a9f3e8ac3b
Bill Barnett <bill@gaslight.co>
2016-12-19 16:46:59 -0500

    Domain validation for marketing.busdetective.com

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
       render text: 'JAb6nuC9AlgQQN0LKe42K1zaNM12DyfaKyRAA4TyCaQ.x2TXuRtPY5PkPL4YMeiKaMl4xBtFrjfOe94AR0Iyg1M'
     when "gTrmpuvlhFtL3v0N2Rkhk9GBxkzXsnkfwyf_-XWRsj0"
       render text: 'gTrmpuvlhFtL3v0N2Rkhk9GBxkzXsnkfwyf_-XWRsj0.3agPbEGMW8yyXAdNJmtYhleq07pUgmnN1oCrhN9iRwA'
+    when 'ErjilUseIpcU9EqaUP2Pz-z48Bqe1DsdU-MbS_zX4eI'
+      render text: 'ErjilUseIpcU9EqaUP2Pz-z48Bqe1DsdU-MbS_zX4eI.x2TXuRtPY5PkPL4YMeiKaMl4xBtFrjfOe94AR0Iyg1M'
     when "OLj4hja-R9m7nOL2UWmj8jG-WlnX7Y2XJqGPEJ_WGXc"
       render text: 'OLj4hja-R9m7nOL2UWmj8jG-WlnX7Y2XJqGPEJ_WGXc.3agPbEGMW8yyXAdNJmtYhleq07pUgmnN1oCrhN9iRwA'
     else
