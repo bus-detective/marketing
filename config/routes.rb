751a64e4833b09479078050fcb483ec385aa0858
Bill Barnett <bill@gaslight.co>
2017-06-20 01:05:19 -0400

    Add convenience link to redirect to Community Discord server

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
+  get '/discord' => redirect('https://discord.gg/FYr5g9a')
   get "/*action", controller: :pages, as: :page
   root to: "pages#home"
 end
